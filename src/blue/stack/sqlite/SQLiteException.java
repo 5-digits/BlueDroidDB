ce632e0129d3a549994356c6c814bc5c7a7e089e
BunnyBlue <bunnyblueair@gmail.com>
2015-01-23 16:55:24 +0800

    a

--- src/blue/stack/sqlite/SQLiteException.java
+++ src/blue/stack/sqlite/SQLiteException.java
@@ -1,20 +1,12 @@
-/*
- * This is the source code of Telegram for Android v. 1.3.2.
- * It is licensed under GNU GPL v. 2 or later.
- * You should have received a copy of the license in this archive (see LICENSE).
- *
- * Copyright Nikolai Kudashov, 2013.
- */
-
 package blue.stack.sqlite;
 
 public class SQLiteException extends Exception {
 	private static final long serialVersionUID = -2398298479089615621L;
 	public final int errorCode;
-	
+
 	public SQLiteException(int errcode, String msg) {
 		super(msg);
-		errorCode = errcode;		
+		errorCode = errcode;
 	}
 
 	public SQLiteException(String msg) {
